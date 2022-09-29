c6b900b2581fd1fb57a74f2b338cca7d4c1b4035
LEESEUNGWON\Leeseungwon <iseungwon239@gmail.com>
2022-09-29 20:27:29 +0900

    달팽이 문제 구현

--- Snail/main.cpp
+++ Snail/main.cpp
@@ -1,92 +1,109 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
 const int MAX = 100;
-int board[MAX][MAX] = {};
-int N;
+int board[MAX][MAX] = {0, };
+int N = 0;
+enum Dir
+{
+	RIGHT = 0,
+	DOWN = 1,
+	LEFT = 2,
+	UP = 3
+};
 
 void PrintBoard()
 {
-	for (int y = 0; y < N; y++)
+	for (int Y = 0; Y < N; Y++)
 	{
-		for (int x = 0; x < N; x++)
+		for (int X = 0; X < N; X++)
 		{
-			cout << setfill('0') << setw(2) << board[y][x] << " ";
+			cout << setfill('0') << setw(2) << board[Y][X] << " ";
 		}
 		cout << endl;
 	}
 }
 
+bool CanGo(int Y, int X)
+{
+	if (Y < 0 || Y >= N)
+	{
+		return false;
+	}
+	if (X < 0 || X >= N)
+	{
+		return false;
+	}
+	if (board[Y][X] != 0)
+	{
+		return false;
+	}
+	return true;
+}
+
 void SetBoard()
 {
+	int Y = 0;
+	int X = 0;
+	int Dir = RIGHT;
 	int Number = 1;
-	int t = 0;
-	for (int x = t; x < N - t; x++)
+	while (true)
 	{
-		board[t][x] = Number;
-		Number++;
-	}
-	for (int y = t; y < N - t - 1; y++)
-	{
-		board[y + 1][N - t - 1] = Number;
-		Number++;
-	}
-	for (int x = N - t - 1; x > t; x--)
-	{
-		board[N - t - 1][x - 1] = Number;
-		Number++;
-	}
-	for (int y = N - t - 2; y > t; y--)
-	{
-		board[y][t] = Number;
-		Number++;
-	}
-	t++;
+		board[Y][X] = Number;
+		if (Number == N * N)
+		{
+			break;
+		}
 
-	for (int x = t; x < N - t; x++)
-	{
-		board[t][x] = Number;
-		Number++;
-	}
-	for (int y = t; y < N - t - 1; y++)
-	{
-		board[y + 1][N - t - 1] = Number;
-		Number++;
-	}
-	for (int x = N - t - 1; x > t; x--)
-	{
-		board[N - t - 1][x - 1] = Number;
-		Number++;
-	}
-	for (int y = N - t - 2; y > t; y--)
-	{
-		board[y][t] = Number;
-		Number++;
-	}
-	t++;
+		int NewY = 0;
+		int NewX = 0;
+		switch (Dir)
+		{
+		case RIGHT:
+			NewY = Y;
+			NewX = X + 1;
+			break;
+		case DOWN:
+			NewY = Y + 1;
+			NewX = X;
+			break;
+		case LEFT:
+			NewY = Y;
+			NewX = X - 1;
+			break;
+		case UP:
+			NewY = Y - 1;
+			NewX = X;
+			break;
+		}
 
-	for (int x = t; x < N - t; x++)
-	{
-		board[t][x] = Number;
-		Number++;
+		if (CanGo(NewY, NewX))
+		{
+			Y = NewY;
+			X = NewX;
+			Number++;
+		}
+		else
+		{
+			switch (Dir)
+			{
+			case RIGHT:
+				Dir = DOWN;
+				break;
+			case DOWN:
+				Dir = LEFT;
+				break;
+			case LEFT:
+				Dir = UP;
+				break;
+			case UP:
+				Dir = RIGHT;
+				break;
+			}
+		}
 	}
-	for (int y = t; y < N - t - 1; y++)
-	{
-		board[y + 1][N - t - 1] = Number;
-		Number++;
-	}
-	for (int x = N - t - 1; x > t; x--)
-	{
-		board[N - t - 1][x - 1] = Number;
-		Number++;
-	}
-	for (int y = N - t - 2; y > t; y--)
-	{
-		board[y][t] = Number;
-		Number++;
-	}
-	t++;
 }
 
 int main()
